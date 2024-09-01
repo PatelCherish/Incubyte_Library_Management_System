72c9bf5c9f1e40b46cf015bce51adeaef79d6903
PatelCherish <131801129+PatelCherish@users.noreply.github.com>
2024-09-01 11:39:45 +0530

    Implemented  method to handle returning borrowed books

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,4 @@
 export { addBook } from './addBook';
 export { borrowBooks } from './borrowBook';
+export { returnBooks } from './returnBook';
+export { viewAvailableBooks } from './viewAvailableBooks';
